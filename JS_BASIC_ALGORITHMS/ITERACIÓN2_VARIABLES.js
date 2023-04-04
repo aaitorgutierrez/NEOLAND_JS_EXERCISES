418b96889c9599faaeb7e568e0f166386a938adc
aaitorgutierrezç <gutierrezizquierdoaitor@gmail.com>
2023-04-04 15:26:28 +0200

    iteration#2.2 Variables avanzadas

---
+++ JS_BASIC_ALGORITHMS/ITERACIÓN2_VARIABLES.js
@@ -0,0 +1,15 @@
+//1.2 Declara 3 variables con los nombres y valores siguientes 
+//	firstName = 'Jon'; 
+//	lastName = 'Snow'; 
+//	age = 24; 
+//Muestralos por consola de esta forma: 
+//	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+
+
+let firstName = "Jon"
+let lastName = "Snow"
+let completeName = firstName + " " + lastName
+let age = 24
+
+console.log("soy " + completeName + ", tengo" + age+ " años y me gustan los lobos")
+
