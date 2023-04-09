16c4b53cc5bf485f82e14ef730cbd399a525d4f8
aaitorgutierrezç <gutierrezizquierdoaitor@gmail.com>
2023-04-09 12:06:59 +0200

    Iteracion_6

---
+++ JS_BASIC_ALGORITHMS/JS_Basic_Algorithms/ITERACIÓN6_VARIABLES.js
@@ -0,0 +1,11 @@
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+for (let i = 1; i <= 9; i++) {
+    console.log(i);
+  }
+
+// 1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+//cuando el resto del numero dividido entre 2 sea 0.
+for (let i = 0; i <= 9; i += 2) {
+    console.log(i)
+}
