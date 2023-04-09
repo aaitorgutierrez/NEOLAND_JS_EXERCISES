396f012eeb452f2bb22870dc2dd4b9999b2ac27e
aaitorgutierrezç <gutierrezizquierdoaitor@gmail.com>
2023-04-09 16:57:41 +0200

    ITERACION6_VARIABLES

--- JS_BASIC_ALGORITHMS/JS_Basic_Algorithms/ITERACIÓN6_VARIABLES.js
+++ JS_BASIC_ALGORITHMS/JS_Basic_Algorithms/ITERACIÓN6_VARIABLES.js
@@ -9,3 +9,17 @@ for (let i = 1; i <= 9; i++) {
 for (let i = 0; i <= 9; i += 2) {
     console.log(i)
 }
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas. 
+//Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+//Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+//y cambia el mensaje en la décima vuelta a 'Dormido!'.
+for (i = 1; i <= 10 ; i++) {
+    if (i <= 9) {
+        console.log("Intentando dormir")
+    } else if (i = 10) {
+        console.log("Dormido")
+    }
+}
+
+
